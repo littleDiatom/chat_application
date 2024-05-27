9e74d018ed6125750ac967ba4cc46810cebbcf70
alifrappe <alizee.d@hotmail.fr>
2024-05-27 10:16:25 +0200

    Nettoyage des dossiers et ajouts de commentaires

--- backend/chatProject/chat/DeleteRoom.py
+++ backend/chatProject/chat/DeleteRoom.py
@@ -2,9 +2,12 @@ from django_cron import CronJobBase, Schedule
 from django.utils import timezone
 from .models import Room
 
-# fonction pour nettoyer régulièrement les salles expirées
+''' 
+Fonction pour nettoyer régulièrement les salles expirées
+Pour le test, la temps limite est fixé à 1 min
+# '''
 class CleanupExpiredRooms(CronJobBase):
-    RUN_EVERY_MINS = 1  #période d'expiration
+    RUN_EVERY_MINS = 1   #Période d'expiration
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'chat.cleanup_expired_rooms' 
--- backend/chatProject/chat/routing.py
+++ backend/chatProject/chat/routing.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from . import consumers
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_name>\w+)/room$", consumers.ChatConsumer.as_asgi()),
 ]
